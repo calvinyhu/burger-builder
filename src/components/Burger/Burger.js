03b09173910ae43a27067295e2972947c4044014
Calvin Hu <cahu@ucdavis.edu>
2018-07-11 21:19:04 -0700

    Dynamic burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,26 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    // An array of strings of the keys of props.ingredients
+    const uniqueIngredients = Object.keys(props.ingredients);
+
+    // An array of all ingredients
+    const allIngredients = uniqueIngredients.map(igKey => {
+        // The count of each unique ingredient
+        const numIngredients = props.ingredients[igKey];
+        // Return an array that is filled with the correct number of unique 
+        // ingredients, by first filling an @Array with @numIngredient number of
+        // undefined values, then mapping over each undefined element and 
+        // turning it into a @BurgerIngredient
+        return [...Array(numIngredients)].map((_, index) => {
+            return <BurgerIngredient key={igKey + index} type={igKey} />;
+        });
+    });
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top' />
-            <BurgerIngredient type='cheese' />
-            <BurgerIngredient type='meat' />
+            {allIngredients}
             <BurgerIngredient type='bread-bottom' />
         </div>
     );
