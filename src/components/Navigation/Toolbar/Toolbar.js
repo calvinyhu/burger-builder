3bb0d861c27e25cd3853e15789cd1d5a124c9afe
Calvin Hu <cahu@ucdavis.edu>
2018-07-12 19:01:10 -0700

    Basic application finished

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,10 +3,13 @@ import React from 'react';
 import classes from './Toolbar.css'
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+// import Button from '../../UI/Button/Button';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
-const toolbar = () => (
+const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
+        {/* <Button className={classes.Menu} clicked={props.drawerToggleClicked}>Menu</Button> */}
+        <DrawerToggle clicked={props.drawerToggleClicked}/>
         <Logo/>
         <nav className={classes.DesktopOnly}>
             <NavigationItems />
