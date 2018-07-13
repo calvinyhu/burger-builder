257d40e57646b1f0a2b089abe52e8389d13c000e
Calvin Hu <cahu@ucdavis.edu>
2018-07-12 19:35:57 -0700

    Optimizations

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Modal.css';
 import Auxiliary from '../../../hoc/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <Auxiliary>
-        <Backdrop show={props.show} clicked={props.modalClosed} />
-        <div style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1': '0',
-            }}
-            className={classes.Modal}>
-            {props.children}
-        </div>
-    </Auxiliary>
-);
+// Did not use PureComponent here, since we do not need to check if modalClosed 
+// has changed
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, _) {
+        return nextProps.show !== this.props.show;
+    }
 
-export default modal;
+    render() {
+        return (
+            <Auxiliary>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1': '0',
+                    }}
+                    className={classes.Modal}>
+                    {this.props.children}
+                </div>
+            </Auxiliary>
+        );
+    }
+}
+
+export default Modal;
