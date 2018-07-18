a86f73eb382d61bcd43d0d0785b8aedff2bc7711
Calvin Hu <cahu@ucdavis.edu>
2018-07-18 13:16:52 -0700

    Added fetch orders

--- src/containers/Checkout/Orders/Orders.js
+++ src/containers/Checkout/Orders/Orders.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
 
 import Order from '../../../components/Order/Order';
+import axios from '../../../axios-orders';
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true,
+    }
+
+    // Only fetch orders when this component is loaded
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(response => {
+                const fetchedOrders = [];
+                for (let key in response.data) {
+                    fetchedOrders.push({
+                        ...response.data[key],
+                        id: key
+                    });
+                }
+                console.log(fetchedOrders);
+                this.setState({loading: false, orders: fetchedOrders});
+            })
+            .catch(error => {
+                this.setState({loading: false});
+            });
+    }
+
     render() {
         return (
             <div>
@@ -13,4 +39,4 @@ class Orders extends Component {
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
