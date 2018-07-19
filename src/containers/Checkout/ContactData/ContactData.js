da04462a94874fd87d59d01193dc12ecff67b0fc
Calvin Hu <cahu@ucdavis.edu>
2018-07-18 18:26:50 -0700

    Modified Input.js

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,11 +8,57 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP Code'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Email'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'}
+                    ]
+                },
+                value: ''
+            },
         },
         loading: false,
     }
@@ -25,17 +71,7 @@ class ContactData extends Component {
             ingredients: this.props.ingredients,
             // On a real app, you would recalculate the price based on the 
             // number of ingredients on the server and not the user
-            price: this.props.price,
-            customer: {
-                name: 'Calvin',
-                address: {
-                    street: 'Street Name',
-                    zipCode: '99999',
-                    country: 'United States',
-                },
-                email: 'calvin@firebase.com',
-            },
-            deliveryMethod: 'fastest',
+            price: this.props.price
         };
         // Comment out this post to see the spinner a bit longer.
         axios.post('/orders.json', order)
@@ -51,7 +87,7 @@ class ContactData extends Component {
     render() {
         let form = (
             <form>
-                <Input inputtype='input' type='text' name='name' placeholder='Name'/>
+                <Input elementType="" elementConfig="..." value=""/>
                 <Input inputtype='input' type='email' name='email' placeholder='Email'/>
                 <Input inputtype='input' type='text' name='street' placeholder='Street'/>
                 <Input inputtype='input' type='text' name='postal' placeholder='Postal'/>
