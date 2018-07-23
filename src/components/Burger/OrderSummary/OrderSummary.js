b7c2f4eadcb299b253058cad9b0a345c89d9c2b7
Calvin Hu <cahu@ucdavis.edu>
2018-07-23 13:52:27 -0700

    Removed console.log(s)

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -5,10 +5,6 @@ import Button from '../../UI/Button/Button';
 
 // This could be a functional component, does not have to be a class
 class OrderSummary extends Component {
-    componentWillUpdate() {
-        console.log('[OrderSummar.js] componentWillUpdate');
-    }
-
     render() {
         const ingredientSummary = Object.keys(this.props.ingredients)
             .map(igKey => {
