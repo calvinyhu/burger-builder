d4bee9855ece815a37848ed6d5292f6cd3c49347
Calvin Hu <cahu@ucdavis.edu>
2018-07-23 17:59:01 -0700

    Added lazy loading

--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
 
 const mapStateToProps = state => {
   return {
@@ -22,6 +20,18 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
+const asyncCheckout = asyncComponent(() => {
+  return import('./containers/Checkout/Checkout')
+})
+
+const asyncOrders = asyncComponent(() => {
+  return import('./containers/Orders/Orders')
+})
+
+const asyncAuth = asyncComponent(() => {
+  return import('./containers/Auth/Auth')
+})
+
 class App extends Component {
   componentDidMount() {
     this.props.onTryAutoSignUp()
@@ -32,7 +42,7 @@ class App extends Component {
     let routes = (
       <Switch>
         <Route path='/burger-builder' component={BurgerBuilder}/>
-        <Route path='/auth' component={Auth}/>
+        <Route path='/auth' component={asyncAuth}/>
         <Redirect to='/burger-builder' />
       </Switch>
     )
@@ -43,9 +53,9 @@ class App extends Component {
       routes = (
         <Switch>
           <Route path='/burger-builder' component={BurgerBuilder}/>
-          <Route path='/checkout' component={Checkout}/>
-          <Route path='/orders' component={Orders}/>
-          <Route path='/auth' component={Auth}/>
+          <Route path='/checkout' component={asyncCheckout}/>
+          <Route path='/orders' component={asyncOrders}/>
+          <Route path='/auth' component={asyncAuth}/>
           <Route path='/logout' component={Logout}/>
           <Redirect to='/burger-builder' />
         </Switch>
